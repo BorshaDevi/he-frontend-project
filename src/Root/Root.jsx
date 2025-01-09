b9370e9e37e3ded0659df075bd9aec83decb175b
unknown <borshadevi27@gmail.com>
2025-01-09 23:21:22 +0600

    add Root file

---
+++ src/Root/Root.jsx
@@ -0,0 +1,8 @@
+const Root =()=>{
+    return (
+        <>
+          <p>Hello world.</p>
+        </>
+    )
+}
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Root from './Root/Root.jsx';
+
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    Component:Root,
+    // children:[
+    //  {
+      
+    //  }
+    // ]
+  }
+])
+
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
