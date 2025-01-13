90f6675f1857552a195fd929069118ec36d0ccac
unknown <borshadevi27@gmail.com>
2025-01-14 00:31:05 +0600

    delete useUser.jsx file

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,19 @@
 import { createContext } from "react";
+import useAxiosPublic from "../hook/useAxiosPublic";
 const AuthProvider=({children})=>{
     const AuthContext=createContext()
-
+    const axiosPublic=useAxiosPublic()
+const logOut=async({Id})=>{
+  await axiosPublic.delete('/logOutUser',Id)
+  .then(res =>{
+   console.log(res)
+  }).catch(error =>{
+   console.log(error)
+  })
+}
 
     const allValue={
-      
+      logOut,
     }
     return(
        <AuthContext.Provider value={allValue}>
